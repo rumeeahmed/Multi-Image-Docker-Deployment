748351f740f81189bc06417dc9e8e7f9bf8773a7
Rumee Ahmed <rumeeahmad@gmail.com>
2021-10-05 10:22:46 +0100

    initial commit

---
+++ worker/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGUSER,
+  pgDatabase: process.env.PGUSER,
+  pgPassword: process.env.POSTGRES_PASSWORD,
+  pgPort: process.env.PGUSER
+};
